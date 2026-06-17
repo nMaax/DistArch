726c3cbb7864ec757e197940f62357e6c0f28c77
nMaax <massimilianocarli@proton.me>
2026-06-17 12:24:57 +0200

    Polished Saprk 2025 02 10

--- Spark/2025_02_10/main.py
+++ Spark/2025_02_10/main.py
@@ -11,75 +11,91 @@ meetings_path = 'meetings.txt'
 invitations_path = 'invitations.txt'
 participations_path = 'participations.txt'
 
+output_dir1 = "/path/to/output1"
+output_dir2 = "/path/to/output2"
+
 # ------------------------------------
 # Part 1
 
-# Dataframes we will need
-meetings = spark.read.csv(meetings_path, header=True, inferSchema=True)
-participations = spark.read.csv(participations_path, header=True, inferSchema=True)
+"""
+Users who frequently organized meetings with the maximum number of allowed
+participants in 2024. The first part of this application selects the users who frequently
+organized meetings in 2024 (meetings with StartTime associated with 2024) with a
+number of actual participants equal to the maximum number of allowed participants
+(MaxParticipants). Specifically, a user is selected if more than 20 of the meetings the
+user organized in 2024 are characterized by a number of distinct actual participants
+equal to the maximum number of allowed participants. A user is considered an actual
+participant in a meeting if he/she participated in the meeting (according to the content
+of Participations.txt). Store the identifiers (UIDs) of the selected users in the first HDFS
+output folder. Specifically, store one UID per output line.
+"""
 
-# NOTE: probably there exist a SQL-native equivalent of this, in such case
-# it would be better to adopt it in place of my udf. I just use UDF to be safe on the result
-# since I can't recall the SQL equivalent right now
-# e.g. .filter("StartTime LIKE '2024%'")
+meetings = spark.read.csv(meetings_path, header=True, inferSchema=True) # (MID,Title,StartTime,EndTime,OrganizerUID,MaxParticipants)
+participations = spark.read.csv(participations_path, header=True, inferSchema=True) # (MID,UID,JoinTimestamp,LeaveTimestamp)
+
+# NOTE: for a SQL-native alternative use:
+#   - .filter("StartTime LIKE '2024%'")
+#   - .filter(YEAR(STR_TO_DATE(StartTime)) = 2024)
+
 def year(timestamp):
     return int(timestamp[:4])
-spark.udf().register("year", year) # automatically infer return type
+spark.udf().register("year", year) # Automatically infer return type
 
-# Filter and clean the data with only what is needed
-meetings_2024 = meetings.filter("year(StartTime) = 2024").select("MID", "OrganizerUID", "MaxParticipants")
+meetings_2024 = (
+    meetings
+    .filter("year(StartTime) = 2024")
+    .select("MID", "OrganizerUID", "MaxParticipants")
+) # (MID, OrganizerUID, MaxParticipants)
 
-# Join on participations with inner join to automatically select only such participations we are interested on
-# to disntinguish between a user who joins multiple times we must run a distinct() on the participations df
-participations_2024 = participations.select("MID", "UID").distinct().join(meetings_2024, on="MID", how="inner")
+participations_2024 = (
+    participations
+    .select("MID", "UID")
+    .distinct() # A user could have joined a meeting multiple times
+    .join(meetings_2024, on="MID", how="inner")
+) # (MID, UID, OrganizerUID, MaxParticipants)
 
-# Aggregate UID for each meeting to get the number of participants
-# since OrganzerUID and MaxParticipants are uniquely associated to MID, this groupBy wont crash
-# as there is not ambiguity
+# NOTE: OrganizerID and MaxParticipants have a functional dependency on MID;
+# for every unique MID, there is exactly one organizer and one maximum participant value.
+# Thus I can include them in the groupBy clause
 actual_participations_2024 = (
     participations_2024
-    .groupBy("MID", "OrganizerID", "MaxParticipants")
+    .groupBy(["MID", "OrganizerID", "MaxParticipants"])
     .count()
-    .withColumnRenamed("count(1)", "ActualParticipants") # this will result in (MID, OrganizerID, MaxParticipants, ActualParticipants)
-)
+    .withColumnRenamed("count(1)", "ActualParticipants")
+) # (MID, OrganizerID, MaxParticipants, ActualParticipants)
 
-# Count how many meetings with actual == max participants, per OrganizerID
 full_meetings_2024 = (
     actual_participations_2024
     .filter("ActualParticipants == MaxParticipants")
     .groupBy("OrganizerUID")
-    .count() # this will result in OrganizerID, numFullMettings
-    .withColumnRenamed("count(1)", "numFullMeetings")
-)
+    .count()
+    .withColumnRenamed("count(1)", "numFullMeetings") #
+) # (OrganizerID, numFullMettings)
 
-# Select IDs of the organizers that exceeded 20 of such meetings, and save the result
-full_meetings_2024.filter("numFullMeetings > 20").select("OrganizerID").write.csv(output_dir1)
+(
+    full_meetings_2024
+    .filter("numFullMeetings > 20")
+    .select("OrganizerID")
+    .write.csv(output_dir1, header=True) # `OrganizerUID` as first line
+)
 
 # ------------------------------------
 # Part 2
 
-# Make a list of meetings where the organizer joined its own meeting, we will subtract this from the whole meetings list
-meetings_organizer_joined_itself = participations_2024.filter("UID = OrganizerUID").select("MID")
+"""
+Participation of the users in the meetings they organized in 2024. The year of interest is
+again 2024. For each user who organized at least one meeting in the year 2024,
+compute the number of meetings he/she organized but did not participate. Store the
+result in the second HDFS output folder. Specifically, there is one output line for each
+user who organized at least one meeting. Each line contains the UID of one of the
+users who organized meetings, followed by the number of meetings UID organized in
+2024 but did not participate in
+"""
 
-# All organizers
-all_organizers = meetings_2024.select("OrganizerUID").distinct()
-
-# left_anti makes us select all the meetings in meetins_2024 that do not appear in meetings_organizer_joined_itself, i.e., meetings where the organizer did NOT joined
-# basically it acts like a subtract
-meetings_organizer_not_joined_itself = (
-    meetings_2024
-    .join(meetings_organizer_joined_itself, on="MID", how="left_anti")
-    .groupBy("OrganizerID")
-    .count()
-    .withColumnRenamed("count(1)", "NumMeetingsOrganizerDidNotJoinedItsOwnMeeting") # this results in Organizer, NumMeetingsOrganizerDidNotJoinedItsOwnMeeting
-    .join(all_organizers, on="OrganizerUID", how="right")
-    .fillna(0) # Those who where missing are those which did join every own organized meeting, thus they count to 0
-)
-
-# Save the result
-meetings_organizer_not_joined_itself.write.csv(output_dir2)
-
-# --- OR --- >>> MORE OPTIMAL <<<
+# NOTE: always prefer re-using already defined dataframes
+# we cannot simply use participations_2024 as it does not include meetings with 0 participations
+# (i.e., nor the organizer, nor anyone else joined it)
+# we need then to join information from meetings_2024 back in.
 
 # Organizer = 1, Participant = 0
 org_df = meetings_2024.selectExpr(
@@ -114,15 +130,14 @@ meetings_with_organizer_presence = (
     #   participations_2024 was built with a distinct() who removed duplicate MID, UID keys; while meetings_2024 has at most
     #   one entry with a given MID (primary key), and of course the associated OrganizerUID
     .filter("sum(is_org)=1") # Select only entries for organizers
-    .selectExpr("UID", "IF(sum(is_part)==0, 1, 0) AS missed") # clean up and rename the columns with something more readable
+    .selectExpr("UID", "IF(sum(is_part)==0, 1, 0) AS missed")
 )
 
 
-tot_missed_own_meetngs_per_organizer = (
+(
     meetings_with_organizer_presence.groupBy("UID")
     .agg({"missed": "sum"})
     .withColumnRenamed("UID", "OrganizerUID")
-    .withColumnRenamed("sum(missed)", "tot_missed")
+    .withColumnRenamed("sum(missed)", "totMissed")
+    .write.csv(output_dir2)
 )
-
-tot_missed_own_meetngs_per_organizer.write.csv(output_dir2)
