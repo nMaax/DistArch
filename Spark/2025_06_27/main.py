958aa1bcbccf2a84b1d9c7b15cf2709913eb2833
nMaax <massimilianocarli@proton.me>
2026-07-03 18:52:25 +0200

    RDD for 2026 06

--- Spark/2025_06_27/main.py
+++ Spark/2025_06_27/main.py
@@ -31,6 +31,36 @@ user type from 2010 to 2020, i.e., no user type is associated with zero
 purchases made by Italian users from 2010 to 2020
 """
 
+# --- RDDs ---
+
+users = sc.textFile(users_path) # (UserID,Age,Gender,Country,UserType)
+purchases = sc.textFile(purchases_path) # (PurchaseID,UserID,ProductID,PurchaseDate)
+
+italian_users = (
+    users
+    .map(lambda line: line.split(",")) # UserID = 0, Age = 1, Gender = 2, Country = 3, UserType = 4
+    .filter(lambda items: items[3] == "Italian") # Select only Country == Italy
+    .map(lambda items: (items[0], items[4])) # Make a pair UserID, UserType
+)
+
+purchases_2010_to_2020 = (
+    purchases
+    .map(lambda line: line.split(","))  # PurchaseID = 0, UserID = 1, ProductID = 2, PurchaseDate = 3
+    .filter(lambda items: 2010 <= int(items[3][:4]) <= 2020) # Select only year betwen 2010 and 2020
+    .map(lambda items: (items[1], 1)) # Make a pair UserID, 1
+)
+
+(
+    italian_users
+    .join(purchases_2010_to_2020) # UserID, (UserType, 1)
+    .map(lambda pair: (pair[1][0], pair[1][1])) # UserType, 1
+    .reduceByKey(lambda a, b: a + b) # UserType, numPurchases (sum of 1s)
+    .map(lambda pair: f"{pair[0]},{pair[1]}")
+    .saveAsTextFile(output_path_1)
+)
+
+# --- DataFrames ---
+
 users = spark.read.csv(users_path, header=True, inferSchema=True) # (UserID,Age,Gender,Country,UserType)
 purchases = spark.read.csv(purchases_path, header=True, inferSchema=True) # (PurchaseID,UserID,ProductID,PurchaseDate)
 
@@ -75,6 +105,40 @@ result is stored in the second output folder. The output contains one line for e
 Italian user without purchases from 2024. The output format is as follows:
 UserID, number of purchases made by UserID in 2023
 """
+
+# --- RDDs ---
+
+users_purchased_from_2024 = (
+    purchases
+    .map(lambda line: line.split(",")) # PurchaseID = 0, UserID = 1, ProductID = 2, PurchaseDate = 3
+    .filter(lambda items: int(items[3][:4]) >= 2024) # Select only years 2024 and beyond
+    .map(lambda items: (items[1], None)) # Make a pair (UserID, None)
+)
+
+italian_users_no_purchases_2024 = (
+    italian_users
+    .subtractByKey(users_purchased_from_2024) # UserID, UserType
+)
+
+purchases_2023 = (
+    purchases
+    .map(lambda line: line.split(",")) # PurchaseID = 0, UserID = 1, ProductID = 2, PurchaseDate = 3
+    .filter(lambda items: int(items[3][:4]) == 2023) # Select only year 2023
+    .map(lambda items: (items[1], 1)) # Make a pair UserID, 1
+    # NOTE: Pre-reducing data before the join will make Spark trigger a shuffle, which will also include users we do not care about,
+    # however, Spark is designed to utilize map-side combining before any data hits the network, thus giving a improvement in optimality
+    .reduceByKey(lambda a, b: a + b) # Pre-sum the numPurchases per user
+)
+
+(
+    italian_users_no_purchases_2024
+    .leftOuterJoin(purchases_2023) # (UserId, (UserType, 1 OR None))
+    .mapValues(lambda value: value[1] if value[1] is not None else 0)
+    .map(lambda pair: f"{pair[0]}, {pair[1]}")
+    .saveAsTextFile(output_path_2)
+)
+
+# --- DataFrames ---
 
 purchases_from_2024 = (
     purchases
