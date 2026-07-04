6d19e2e89b95dfbc7f56a332b797f054f543b3dd
nMaax <massimilianocarli@proton.me>
2026-07-04 19:18:45 +0200

    RDDs for 2024 07

--- Spark/2024_07_19/main.py
+++ Spark/2024_07_19/main.py
@@ -23,50 +23,53 @@ selects the courses with a percentage of long lectures greater than 70%. A lectu
 is classified as a long lecture if it lasts at least 120 minutes. Store the identifiers
 (CIDs) of the selected courses in the first output folder (one selected CID per output
 line).
+
+Note: Suppose there is at least one lecture for each course
 """
 
-# Remind, there is at least one leacture for each course, so we dont have to find
-# quirky courses (anyway, they would be intractable: would their ratio be 0% or 100% ?)
+# --- RDDs ---
+
+lectures = sc.textFile(lectures_path) # (NUML,CID,Title,Date,StartingHour,Duration,Recorded)
+
+(
+    lectures
+    .map(lambda line: line.split(","))
+    .map(lambda items: (items[1], (1 if float(items[5]) >= 120 else 0, 1))) # CID, ({0, 1} (Counter for long lencture), 1 (Counter for lecture))
+    .reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) # CID, (NumLongLecture, NumLectures)
+    .filter(lambda pair: pair[1][0] / pair[1][1] > 0.7) # NumLongLecture/NumLectures > 0.7
+    .keys()
+    .saveAsTextFile(output_path_1)
+)
+
+# --- DataFrames ---
+
 
 lectures = spark.read.csv(lectures_path, header=True, inferSchema=True) # (NUML,CID,Title,Date,StartingHour,Duration,Recorded)
 
 (
     lectures
-    .selectExpr("CID", "CASE WHEN Duration > 120 THEN 1 ELSE 0 END AS isLong") # (CID, isLong)
+    .selectExpr("CID", "CASE WHEN Duration >= 120 THEN 1 ELSE 0 END AS isLong") # (CID, isLong)
     .groupBy("CID")
     .agg({"*": "count", "IsLong": "sum"})
     .selectExpr("CID", "`sum(isLong)` / `count(*)` AS longRatio") # Or count(1), whatever it is called
-    .filter("longRatio >= 0.7")
+    .filter("longRatio > 0.7")
     .select("CID")
     .write.csv(output_path_1, header=True)
 )
 
-# ---- OR ----
+# --- Spark SQL ---
 
-def get_cid_longflag(line: str):
-    # key = CID
-    # value = (+1/0, +1)
-    # +1 if it's a long lecture, 0 otherwise
-    fields = line.split(',')
-    cid = fields[1]
-    duration = int(fields[-2])
-    return (cid, (1 if duration > 120 else 0, 1))
+# Register the DataFrame as an accessible SQL table
+lectures.createOrReplaceTempView("lectures_table")
 
-# get for each lecture the CID and a flag: +1 if long, 0 otherwise,
-# and use a reduceByKey to sum, for each CID, its total duration
-# and finally use a mapValues to compute the percentage of long lectures
-# key = CID
-# value = percentage of long lectures
-# To conclude, the filter on 70% threshold is applied
-long_lecture_percentage = lectures_rdd \
-                    .map(get_cid_longflag) \
-                    .reduceByKey(lambda i1, i2: (i1[0] + i2[0], i1[1] + i2[1])) \
-                    .filter(lambda x: x[1][0] / x[1][1] > 0.7)
+part1_sql = """
+    SELECT CID
+    FROM lectures_table
+    GROUP BY CID
+    HAVING SUM(CASE WHEN Duration >= 120 THEN 1 ELSE 0 END) / COUNT(*) > 0.7
+"""
 
-# obtain the CIDs associated to courses with at least 70% of long lectures
-# and save them
-res1 = long_lecture_percentage.keys()
-res1.saveAsTextFile('output1')
+spark.sql(part1_sql).write.csv(output_path_1, header=True)
 
 # ------------------------------------
 # Part 2
@@ -81,6 +84,61 @@ student, consider only the courses for which the student watched at least one
 recorded lecture. Store the result in the second output folder (one of the selected
 combinations (SID, CID) per output line).
 """
+
+# --- RDDs ---
+
+user_watched_lecture = sc.textFile(user_watched_lecture_path) # (SID,StartWatchingTime,NUML,CID)
+
+def all_unique_lectures(pair):
+    SID, CID = pair[0]
+    lectures = pair[1]
+
+    lectures_list = list(lectures)
+    lectures_set = set(lectures)
+
+    if len(lectures_list) == len(lectures_set):
+        return True
+    else:
+        return False
+
+(
+    user_watched_lecture
+    .map(lambda line: line.split(","))
+    .map(lambda items: ((items[0], items[3]), items[2])) # (SID, CID), NUML
+    .groupByKey()
+    .filter(all_unique_lectures)
+    .keys()
+    .saveAsTextFile(output_path_2)
+)
+
+# However, this means that the groupBy will include a large amount of numbers which will be stored in main memory,
+# thus, highly active students will end up clogging the worknode; we can rather do
+
+invalid_SID_CID = (
+    user_watched_lecture
+    .map(lambda line: line.split(","))
+    .map(lambda items: ((items[0], items[3], items[2]), 1)) # (SID, CID, NUML), 1
+    .reduceByKey(lambda a, b: a+b)
+    .filter(lambda pair: pair[1] > 1)
+    .map(lambda pair: (pair[0][0], pair[0][1])) # SID, CID
+)
+
+(
+    user_watched_lecture
+    .map(lambda line: line.split(","))
+    .map(lambda items: (items[0], items[3])) # SID, CID
+    # NOTE: We could call distinct either before (left and/or right sidewise) or after the subtraction, leading to 4 scenarios:
+    # Only After: [A, A, B, B, B, C, C, D, E, F, F] \ [A, B, B, D, F, F] = [C, C, E] on which we would call distinct and get [C, E]
+    # Before, left only: [A, B C, D, E, F] \ [A, B, B, D, F, F] = [C, E]
+    # Before, right only: [A, A, B, B, B, C, C, D, E, F, F] \ [A, B, D, F] = [C, C, E] then we would need to call it again
+    # Before, both right and left: [A, B, C, D, E, F] \ [A, B, D, F] = [C, E]
+    # Among all options better to do distinct BEFORE the subtract for saving network resources for the subtraction
+    .distinct()
+    .subtract(invalid_SID_CID)
+    .saveAsTextFile(output_path_2)
+)
+
+# --- DataFrames ---
 
 user_watched_lecture = spark.read.csv(user_watched_lecture_path, header=True, inferSchema=True) # (SID,StartWatchingTime,NUML,CID)
 
@@ -105,7 +163,7 @@ too_many_times_watched_course_lecture = (
     times_watched_recorded_lecture_per_student
     .join(
         too_many_times_watched_course_lecture,
-        on=["SID", "CID"]
+        on=["SID", "CID"],
         how="anti"
         # Remove entries in times_watched_recorded_lecture_per_student that appear in too_many_times_watched_course_lecture
     )
@@ -123,41 +181,30 @@ too_many_times_watched_course_lecture = (
 #       If you used the select().subtract() approach shown above,
 #       you wouldn't even need the final .distinct() call at the end
 #       of your chain. The output of subtract() is already guaranteed to be distinct.
+#   -   We dont need to care about calling .distinct() on left or right of the anti join as Spark Catalyst Optimizer handles it for us
+#       (we can simply call it later)
 
-# ---- OR ----
+# --- SparkSQL ---
 
-# Retrieve for each student the watched recorded lectures.
-# All lectures in UsersWatchedRecordedLectures.txt are recorded lectures by definition.
-# A join with lectures is not needed.
+# Register the raw watch log DataFrame as an accessible SQL table
+user_watched_lecture.createOrReplaceTempView("watches_table")
 
-# Count the number of times each recorded lecture was watched by each student
-# key = SID, NumL, CID
-# value = number of times the lecture was watched by student SID
-def get_sid_cid_numl(s: str):
-    fields = s.split(',')
-    sid = fields[0]
-    numl = fields[2]
-    cid = fields[3]
+part2_sql = """
+    -- STEP 1: Get all unique pairs where a student watched at least one lecture
+    SELECT DISTINCT SID, CID
+    FROM watches_table
 
-    return ((sid, numl, cid), 1)
+    EXCEPT
 
-sid_lecture_count = watched_lectures_rdd.map(get_sid_cid_numl) \
-                    .reduceByKey(lambda x1, x2: x1 + x2).cache()
+    -- STEP 2: Remove pairs where ANY single lecture was watched more than once
+    SELECT SID, CID
+    FROM (
+        SELECT SID, CID, NUML, COUNT(*) as watch_count
+        FROM watches_table
+        GROUP BY SID, CID, NUML
+    )
+    WHERE watch_count > 1
+"""
 
-
-# For each students, select the lectures watched more than one time.
-# Those are used to identify the combinations (sid, cid) to discard
-# Determine the invalid combinations with a filter and keep sid and cid only
-invalid_cid_sid = sid_lecture_count.filter(lambda x: x[1] > 1)\
-                                    .map(lambda p: (p[0][0], p[0][2]))
-
-
-# Select the distinct combinations (sid, cid) for which sid watched at least one recorded lecture of cid
-sid_watched_cid = sid_lecture_count.map(lambda p: (p[0][0], p[0][2]))\
-                                    .distinct()
-
-# Remove from sid_watched_cid the invalid combinations (sid, cid)
-# and extract the keys
-res2 = sid_watched_cid\
-            .subtract(invalid_cid_sid)
-res2.saveAsTextFile('output2')
+# Execute and write out
+spark.sql(part2_sql).write.csv(output_path_2, header=True)
